b09707933937c9d3d7937a81ed39862f6eaf2151
manuel@betangible.com <rivendel76>
2018-12-20 09:36:47 +0100

    commit inicial

---
+++ src/main/java/com/at/oauth2example/Oauth2ExampleApplication.java
@@ -0,0 +1,14 @@
+package com.at.oauth2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ExampleApplication.class, args);
+    }
+
+}
+
